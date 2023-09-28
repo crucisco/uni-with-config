d5a30d982cac2d1ddf719d814d32a61754892550
Daishik <crucisconsulting@outlook.com>
2023-09-28 21:01:54 +0100

    Managed to figure out how to get server-side config loaded
    #148

--- src/app/services/app-config.service.ts
+++ src/app/services/app-config.service.ts
@@ -1,32 +1,47 @@
 import { Injectable } from '@angular/core';
 import { AppConfig } from '../app.config';
+import { isBrowser } from 'browser-or-node';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppConfigService {
+  private configFile: string = '../../assets/config.json';
   private appConfig: AppConfig = new AppConfig();
 
   constructor() {
   }
 
   loadAppConfig() {
-    var configFile = '../../assets/config.json'
-    return fetch(configFile)
+    if (isBrowser) {
+      return this.loadConfigForBrowser();
+    }
+    else {
+      return this.loadConfigForNode();
+    }
+  }
+
+  getConfig(): AppConfig {
+    return this.appConfig;
+  }
+
+  private loadConfigForBrowser(): Promise<void> {
+    return fetch(this.configFile)
       .then((response) => {
-        console.debug(`Fetched data from '${configFile}'`);
+        console.debug(`Browser fetch config data from '${this.configFile}'`);
         return response.json();
       })
       .then((config) => {
         this.appConfig = config;
         console.debug(`App config env: ${this.appConfig.env}`);
-      })
-      .finally(() => {
-        console.debug("All done");
       });
   }
 
-  getConfig(): AppConfig {
-    return this.appConfig;
+  private loadConfigForNode(): Promise<void> {
+    let config = require('../../assets/config.json');
+    console.debug(`Node server load data from '${this.configFile}'`);
+    this.appConfig = config;
+    console.debug(`App config env: ${this.appConfig.env}`);
+    return Promise.resolve();
   }
 }
